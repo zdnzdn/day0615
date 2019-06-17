af4329a6985ddd0c628f9fc462a0f4eb69522b20
GS <2669433022@qq.com>
2019-06-17 10:20:19 +0800

    first add

--- src/main/java/com/baidu/day0615/Day0615Application.java
+++ src/main/java/com/baidu/day0615/Day0615Application.java
@@ -3,9 +3,11 @@ package com.baidu.day0615;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.baidu.day0615.dao")
+@EnableFeignClients
 public class Day0615Application {
 
 	public static void main(String[] args) {
